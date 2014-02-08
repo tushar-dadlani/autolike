7675ee72aa9c8ba5f2d992fc5ecd010f3b20f0a4
Tushar Dadlani <tush726@gmail.com>
2014-02-08 04:19:55 -0800

    basic reading of signals to the log

--- src/com/example/OpenWave/LightswitchActivity.java
+++ src/com/example/OpenWave/LightswitchActivity.java
@@ -1,21 +1,15 @@
 package com.example.OpenWave;
 
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.neurosky.thinkgear.TGDevice;
@@ -23,36 +17,18 @@ import com.neurosky.thinkgear.TGDevice;
 public class LightswitchActivity extends Activity {
 	TGDevice tgDevice;
 	BluetoothAdapter bluetoothAdapter;
-	ImageView shine0;
-	ImageView shine1;
-	SeekBar seekbar;
+
 	final boolean rawEnabled = false;
-	TextView status;
-	Timer timer;
-	double delta;
+
+
 	double target;
 
 	@SuppressLint("NewApi") @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.lightswitch);
 
-		View v = findViewById(R.id.lightbulboff);
-		View root = v.getRootView();
-		root.setBackgroundColor(Color.BLACK);
 
-		
-		ImageView lightbulb = (ImageView) findViewById(R.id.LightBulbon);
-		lightbulb.setImageResource(R.drawable.lightbulbon);
-		
-		final ImageView on = (ImageView) findViewById(R.id.LightBulbon);
 
-		status = (TextView) findViewById(R.id.status);
-		Typeface font = Typeface.createFromAsset(getAssets(), "BebasNeue.otf");
-		status.setTypeface(font);
-		status.setBackgroundColor(Color.BLACK);
-		status.setTextColor(Color.WHITE);
-		
 		// Connect the device Via Bluetooth
 		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if(bluetoothAdapter == null) {
@@ -64,34 +40,10 @@ public class LightswitchActivity extends Activity {
 		}          
 
 		if(tgDevice.getState() != TGDevice.STATE_CONNECTING && tgDevice.getState() != TGDevice.STATE_CONNECTED){
-			tgDevice.connect(rawEnabled);   
+			tgDevice.connect(rawEnabled);
 		}	
 
-		//Set seekbar changeListener
-		seekbar = (SeekBar) findViewById(R.id.seekBar1);
-		seekbar.setBackgroundColor(Color.BLACK);
-		seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
-			@SuppressLint("NewApi") 
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				on.setAlpha((float) progress/100);
-			}
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {}
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {}
-		});
-		
-		timer = new Timer();
-		timer.schedule(new TimerTask(){
-			public void run() {
-				int current = seekbar.getProgress();
-				delta = Math.pow((Math.abs(target - current)),.25);
-				if(target < current) delta *= -1;
-				seekbar.setProgress(seekbar.getProgress() + (int) delta);
-			}
-		}, 10, 30);
 	}
 
 	@Override
@@ -102,8 +54,6 @@ public class LightswitchActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.lightswitch, menu);
 		return true;
 	}
 
@@ -111,39 +61,59 @@ public class LightswitchActivity extends Activity {
 		@SuppressLint("NewApi") @Override
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
-			case TGDevice.MSG_ATTENTION:	
+			case TGDevice.MSG_MEDITATION:
+				Log.v("Meditation",Integer.toString(msg.arg1));
+				break;
+			case TGDevice.MSG_ATTENTION:
+				Log.v("Attention",Integer.toString(msg.arg1));
 				target = msg.arg1;
 				break;
 			case TGDevice.MSG_BLINK:
-				System.out.println("Blink: " + msg.arg1);
-
-				//Blink.setText("Blink: " + msg.arg1 + "");
+				Log.v("Blink", Integer.toString(msg.arg1));
 				break;
 			case TGDevice.MSG_STATE_CHANGE:
 				switch (msg.arg1) {
 				case TGDevice.STATE_IDLE:
-					status.setText("Status: Idle");
+					Log.v("Status", "Idle");
 					break;
 				case TGDevice.STATE_CONNECTING:		                	
-					status.setText("Status: Connecting...");
+					Log.v("Status", "Connecting...");
 					break;		                    
 				case TGDevice.STATE_CONNECTED:
-					status.setText("Status: Connected.");
+					Log.v("Status", "Connected.");
 					tgDevice.start();
 					break;
 				case TGDevice.STATE_NOT_FOUND:
-					status.setText("Status: Can't find");
+					Log.v("Status", "Can't find");
 					break;
 				case TGDevice.STATE_NOT_PAIRED:
-					status.setText("Status: not paired");
+					Log.v("Status", "not paired");
 					break;
 				case TGDevice.STATE_DISCONNECTED:
-					status.setText("Status: Disconnected mang");
+					Log.v("Status", "Disconnected mang");
 				}
 				break;
-			default:
+
+
+			case TGDevice.MSG_POOR_SIGNAL:
+				Log.v("Control", "PoorSignal: " + msg.arg1); 
 				break;
+			case TGDevice.MSG_RAW_DATA:
+				int rawValue = msg.arg1;
+				Log.v("RAW",Integer.toString(rawValue));
+				break;
+			case TGDevice.MSG_EEG_POWER:
+				int ep = msg.arg1;
+				Log.v("HelloEEG", "Delta: " + Integer.toString(ep));
+				break;
+
+			default:
+				break; 
 			}
+
+
+
+
 		}
 	};
 }
